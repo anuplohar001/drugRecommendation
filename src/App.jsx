6bcf7459610e450dd880b30d546e5e224f5d9cc5
anuplohar001 <anuplohar001@gmail.com>
2025-02-11 10:58:42 +0530

    .jsxAdd2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import DrugAvailability from './components/DrugAvailability.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import { AuthProvider } from "./components/AuthContext.jsx";
-import MyProfile from './components/Myprofile.jsx';
+import MyProfile from './components/MyProfile.jsx';
 
 function App() {  
 
