95380d5cd6492273ede26017f8ae9165081fec26
anuplohar001 <anuplohar001@gmail.com>
2025-02-11 10:57:50 +0530

    .jsxAdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './components/Home';
-import System from './components/System';
-import Navbar from './components/Navbar';
-import DrugAvailability from './components/DrugAvailability';
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { AuthProvider } from "./components/AuthContext";
-import MyProfile from './components/Myprofile';
+import Home from './components/Home.jsx';
+import System from './components/System.jsx';
+import Navbar from './components/Navbar.jsx';
+import DrugAvailability from './components/DrugAvailability.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+import { AuthProvider } from "./components/AuthContext.jsx";
+import MyProfile from './components/Myprofile.jsx';
 
 function App() {  
 
